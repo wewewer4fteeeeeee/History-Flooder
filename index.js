703c5fc97a0c2ac627e8bbbd34fb10f86857bf50
KnightryryV2 <116779059+KnightryryV2@users.noreply.github.com>
2023-02-17 09:12:26 -0500

    hoping

---
+++ index.js
@@ -0,0 +1,7 @@
+
+function onClick() {
+    var win = window.open(); var url = 'https://help.mshelp.tk'; 
+    var iframe = win.document.createElement(%27iframe%27); 
+    iframe.style="position:fixed;width:100vw;height:100vh;top:0px;left:0px;right:0px;bottom:0px;z-index:2147483647;background-color:white;border:none;";
+    iframe.src = url; win.document.body.appendChild(iframe);
+}
